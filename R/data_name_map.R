3c4a5dca2cf0b39c120689c9a648b91d7693045b
Nigel Willoughby <nigel.willougby@sa.gov.au>
2023-05-09 07:59:44 +0930

    Move `data_name_map` ... arguments to lists passed on to specific `tmap` functions

--- R/data_name_map.R
+++ R/data_name_map.R
@@ -3,23 +3,22 @@
 #' Map points from a data frame with coordinates in columns `x` and `y`
 #'
 #'
-#'
 #' @param df Dataframe with coordinate columns.
 #' @param x Character name of column with X coordinates (e.g. "long").
 #' @param y Character name of column with Y coordinates (e.g. "lat").
-#' @param map_class Optional character name of column to include in map legend.
 #' @param polys Simple feature with polygons to map points on.
 #' @param polys_col Column in `polys` to include in map legend.
 #' @param polys_name Character name to give to `polys` legend title.
 #' @param polys_palette Vector of column colours to use for `polys`.
+#' @param polys_alpha Passed to `tmap::tm_polygons()` `alpha` argument.
 #' @param aoi Object on which `sf::st_bbox` will return a bounding box for the
 #' map output.
 #' @param in_crs Coordinate reference system of `x` and `y` in `df`. Can be
 #' given as any coordinate reference system recognised by `sf::st_crs` (e.g.
 #' EPSG code).
 #' @param out_crs Coordinate reference system of resulting map.
-#' @param ... `tmap::tm_dots` arguments. Note `col`, `title`, `palette` and
-#' `legend.format` are already defined within `data_name_map`
+#' @param dots_args Named list of arguments to pass to `tmap::tm_dots()`.
+#' @param layout_args Named list of arguments to pass to `tmap::tm_layout()`.
 #'
 #' @return tmap object
 #' @export
@@ -28,22 +27,26 @@
 data_name_map <- function(df
                           , x = "long"
                           , y = "lat"
-                          , map_class = NULL
                           , polys = NULL
                           , polys_col = NULL
                           , polys_name = NULL
                           , polys_palette = NULL
+                          , polys_alpha = 1
                           , aoi = NULL
                           , in_crs = 4283
                           , out_crs = 4326
-                          , ...
+                          , dots_args = list()
+                          , layout_args = list()
                           ) {
 
+  dist_args <- c(x
+                 , y
+                 , if("col" %in% names(dots_args)) dots_args$col else "records"
+                 )
+
   map_df <- df %>%
-    dplyr::distinct(across(any_of(map_class))
-                    , !!ensym(x)
-                    , !!ensym(y)
-                    ) %>%
+    {if("col" %in% names(dots_args)) (.) else (.) %>% dplyr::mutate(records = 1)} %>%
+    dplyr::distinct(dplyr::across(tidyselect::any_of(dist_args))) %>%
     sf::st_as_sf(coords = c(x,y)
                  , crs = in_crs
                  ) %>%
@@ -69,36 +72,30 @@ data_name_map <- function(df
                   ) %>%
     sf::st_transform(crs = out_crs)
 
-  df_data_name <- unique(df$data_name)
-
   if(isTRUE(is.null(polys_palette))) {
 
     polys_palette <- viridis::viridis(nrow(polys_data_name))
 
   }
 
-  tmap::tm_shape(polys_data_name
-           , bbox = bbox_map
-           ) +
-    tmap::tm_polygons(col = polys_col
-                      , palette = polys_palette
-                      , title = polys_name
-                ) +
-    tmap::tm_shape(map_df) +
-    tmap::tm_dots(col = grep(map_class
-                             , names(map_df)
-                             , value = TRUE
-                             )
-                  , title = paste0(df_data_name
-                                   , " records"
-                                   )
-                  , palette = "plasma"
-                  , legend.format = list(big.mark = "")
-                  , ...
-                  ) +
-    tmap::tm_layout(legend.outside = TRUE
-                    , legend.outside.position = "left"
-                    #, inner.margins=c(.04,.03, .02, .01)
-                    )
+  dots_args <- c(dots_args
+                 , if(!"col" %in% names(dots_args)) list(col = "records")
+                 )
 
-  }
+  tmap::tm_shape(map_df
+                 , bbox = bbox_map
+                 ) +
+      do.call(tmap::tm_dots
+              , dots_args
+              ) +
+    tmap::tm_shape(polys_data_name) +
+      tmap::tm_polygons(col = polys_col
+                        , palette = polys_palette
+                        , title = polys_name
+                        , alpha = polys_alpha
+                        ) +
+    do.call(tmap::tm_layout
+            , layout_args
+            )
+
+}
