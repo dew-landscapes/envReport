f518b3829f3417586470f1a7517335cc3b44cecb
Acanthiza <nigel.willoughby@sa.gov.au>
2025-05-06 14:30:06 +0930

    source of field. 'document()'

--- R/source_of_field.R
+++ R/source_of_field.R
@@ -6,16 +6,17 @@
 #'
 #' @param data_map Dataframe mapping original field to unified fields
 #' @param this_col Which field (column) should be summarised here.
+#' @param first_data_col First column in data_map containing data_names.
 #'
 #' @return text string
 #' @export
 #'
 #' @examples
-source_of_field <- function(data_map, this_col) {
+source_of_field <- function(data_map, this_col, first_data_col = 2) {
 
   data_map %>%
     dplyr::filter(col == this_col) %>%
-    tidyr::pivot_longer(4:ncol(.)) |>
+    tidyr::pivot_longer(first_data_col:ncol(.)) |>
     na.omit() |>
     dplyr::mutate(text = paste0("`"
                                 , name
