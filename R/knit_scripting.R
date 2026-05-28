55042f84382b45e0fabfb66e928cb9ff22a1af49
acanthiza <nigel.willoughby@sa.gov.au>
2026-05-28 13:10:00 +0930

    simplify 'scripting'. Also close #3

--- R/knit_scripting.R
+++ R/knit_scripting.R
@@ -1,34 +1,21 @@
 #' Insert the scripted workflow description common to most projects
 #'
-#' Describes R/RStudio, git, packages, targets, and reporting. Also introduces `envBlah` packages, pulling from packages_bib_file
+#' Describes R/RStudio, git, packages, targets, and reporting. Also introduces
+#' `envBlah` packages, pulling from packages_bib_file
 #'
-#' @param pkg_bib_file *Absolute* file path to packages.bib (e.g. in report_prep store). Usually needs additional dots .. when run inside a report.
+#' @param packages Character. Vector of any loaded packages (doesn't have to
+#' be limited to just `envBlah` packages).
 #'
 #' @returns Markdown child document.
 #' @export
 #'
-knit_scripting <- function(pkg_bib_file) {
-
-  if(!fs::is_absolute_path(pkg_bib_file)) stop("Ensure pkg_bib_file is an absolute path (and remember to add additional dots .. where necessary)")
-
-  if(!file.exists(pkg_bib_file)) stop("pkg_bib_file not found at ", pkg_bib_file)
-
-  env_packages_txt <- bib2df::bib2df(pkg_bib_file) |>
-    dplyr::filter(grepl("^R-env", BIBTEXKEY)) |>
-    dplyr::select(BIBTEXKEY,TITLE) |>
-    dplyr::rowwise() |>
-    dplyr::mutate(usetitle = paste0(
-      "*",
-      gsub("^R-", "", BIBTEXKEY),
-      "*",
-      gsub("^env.*:", ":", TITLE)
-      )) |>
-    dplyr::pull(usetitle) |>
-    paste0(collapse = "\n-   ")
-
-  knitr::knit_child(system.file("rmd/scripting.Rmd", package = "envReport"),
-                    envir = environment(),
-                    quiet = TRUE) |>
+knit_scripting <- function(packages = sort(unique(unname(unlist(yaml::read_yaml(here::here("settings", "packages.yaml"))))))) {
+  
+  knitr::knit_child(system.file("rmd/scripting.Rmd"
+                                , package = "envReport"
+                                )
+                    , quiet = TRUE
+                    ) |>
     cat()
 
 }
